569624344938cb01d1cef23c97e53fc17b7bbf90
yindaheng98 <yindaheng98@gmail.com>
2022-12-06 22:59:31 +0800

    HalfIfaceSetMapaMteS

--- halfifacesetmap.go
+++ halfifacesetmap.go
@@ -2,54 +2,73 @@ package setmap
 
 // HalfIfaceSetMap is map<K, set<V>>
 type HalfIfaceSetMap[K, V comparable] struct {
-	m map[K]map[V]IfaceSetMapItem[V]
+	setmap IfaceSetMap[K, V]
 }
 
 func NewHalfIfaceSetMap[K, V comparable]() HalfIfaceSetMap[K, V] {
 	return HalfIfaceSetMap[K, V]{
-		m: make(map[K]map[V]IfaceSetMapItem[V]),
+		setmap: NewIfaceSetMap[K, V](),
 	}
 }
 
 // Add a value to the set whose ID is key
 func (m HalfIfaceSetMap[K, V]) Add(key K, value IfaceSetMapItem[V]) {
-	if set, ok := m.m[key]; ok {
-		set[value.ID()] = value
-	} else {
-		m.m[key] = map[V]IfaceSetMapItem[V]{value.ID(): value}
-	}
+	m.setmap.Add(SimpleIfaceSetMapItem[K]{T: key}, value)
 }
 
 // Remove a value from the set whose ID is key
 func (m HalfIfaceSetMap[K, V]) Remove(key K, value IfaceSetMapItem[V]) {
-	if set, ok := m.m[key]; ok {
-		delete(set, value.ID())
-		if len(set) <= 0 {
-			delete(m.m, key)
-		}
-	}
+	m.setmap.Remove(SimpleIfaceSetMapItem[K]{T: key}, value)
 }
 
 // Exists show if a value is in the set whose ID is key
 func (m HalfIfaceSetMap[K, V]) Exists(key K, value IfaceSetMapItem[V]) bool {
-	var set map[V]IfaceSetMapItem[V]
-	var ok bool
-	if set, ok = m.m[key]; ok {
-		_, ok = set[value.ID()]
-	}
-	return ok
+	return m.setmap.Exists(SimpleIfaceSetMapItem[K]{T: key}, value)
 }
 
 // GetSet get all the values from the set whose ID is key
 func (m HalfIfaceSetMap[K, V]) GetSet(key K) []V {
-	var r []V
-	if set, ok := m.m[key]; ok {
-		r = make([]V, len(set))
-		i := 0
-		for value := range set {
-			r[i] = value
-			i++
-		}
+	return m.setmap.GetSet(SimpleIfaceSetMapItem[K]{T: key})
+}
+
+// HalfIfaceSetMapaMteS consists of a IfaceSetMap map<K, set<V>> and a reverse IfaceSetMap map<V, set<K>>
+// so it can show K和V之间的多对多关系
+type HalfIfaceSetMapaMteS[K, V comparable] struct {
+	setmapamtes IfaceSetMapaMteS[K, V]
+}
+
+func NewHalfIfaceSetMapaMteS[K, V comparable]() HalfIfaceSetMapaMteS[K, V] {
+	return HalfIfaceSetMapaMteS[K, V]{
+		setmapamtes: NewIfaceSetMapaMteS[K, V](),
 	}
-	return r
+}
+
+func (m HalfIfaceSetMapaMteS[K, V]) Add(key K, value IfaceSetMapItem[V]) {
+	m.setmapamtes.Add(SimpleIfaceSetMapItem[K]{T: key}, value)
+}
+
+func (m HalfIfaceSetMapaMteS[K, V]) Remove(key K, value IfaceSetMapItem[V]) {
+	m.setmapamtes.Remove(SimpleIfaceSetMapItem[K]{T: key}, value)
+}
+
+func (m HalfIfaceSetMapaMteS[K, V]) RemoveValue(value IfaceSetMapItem[V]) {
+	m.setmapamtes.RemoveValue(value)
+}
+
+func (m HalfIfaceSetMapaMteS[K, V]) RemoveKey(key K) {
+	m.setmapamtes.RemoveKey(SimpleIfaceSetMapItem[K]{T: key})
+}
+
+func (m HalfIfaceSetMapaMteS[K, V]) GetKeys(value IfaceSetMapItem[V]) []K {
+	return m.setmapamtes.GetKeys(value)
+}
+
+// GetUniqueValues only get the key's values that is not exists in other key's set
+func (m HalfIfaceSetMapaMteS[K, V]) GetUniqueValues(key K) []V {
+	return m.setmapamtes.GetUniqueValues(SimpleIfaceSetMapItem[K]{T: key})
+}
+
+// GetUniqueKeys only get the value's keys that is not other value's key
+func (m HalfIfaceSetMapaMteS[K, V]) GetUniqueKeys(value IfaceSetMapItem[V]) []K {
+	return m.setmapamtes.GetUniqueKeys(value)
 }
--- halfifacesetmap_test.go
+++ halfifacesetmap_test.go
@@ -32,3 +32,32 @@ func TestHalfIfaceSetMap(t *testing.T) {
 	t.Log(sm.Exists("2", SimpleIfaceSetMapItem[int]{2}))
 	sm.Add("2", SimpleIfaceSetMapItem[int]{3})
 }
+
+func TestHalfIfaceSetMapaMteS(t *testing.T) {
+	sm := NewHalfIfaceSetMapaMteS[string, int]()
+	sm.Add("1", SimpleIfaceSetMapItem[int]{1})
+	sm.Add("1", SimpleIfaceSetMapItem[int]{2})
+	sm.Add("1", SimpleIfaceSetMapItem[int]{3})
+	sm.Add("2", SimpleIfaceSetMapItem[int]{1})
+	sm.Add("2", SimpleIfaceSetMapItem[int]{2})
+	sm.Add("2", SimpleIfaceSetMapItem[int]{3})
+	t.Log(sm.GetUniqueKeys(SimpleIfaceSetMapItem[int]{2}))
+	t.Log(sm.GetUniqueValues("2"))
+	sm.Remove("1", SimpleIfaceSetMapItem[int]{2})
+	t.Log(sm.GetUniqueValues("2"))
+	sm.Remove("2", SimpleIfaceSetMapItem[int]{1})
+	sm.Add("4", SimpleIfaceSetMapItem[int]{2})
+	sm.Add("2", SimpleIfaceSetMapItem[int]{4})
+	t.Log(sm.GetUniqueKeys(SimpleIfaceSetMapItem[int]{2}))
+	t.Log(sm.GetUniqueValues("2"))
+
+	sm.Add("1", SimpleIfaceSetMapItem[int]{1})
+	sm.Add("1", SimpleIfaceSetMapItem[int]{2})
+	sm.Add("1", SimpleIfaceSetMapItem[int]{3})
+	sm.Add("2", SimpleIfaceSetMapItem[int]{1})
+	sm.Add("2", SimpleIfaceSetMapItem[int]{2})
+	sm.Add("2", SimpleIfaceSetMapItem[int]{3})
+	sm.RemoveKey("2")
+	sm.RemoveValue(SimpleIfaceSetMapItem[int]{2})
+	t.Log(sm.GetUniqueValues("1"))
+}
